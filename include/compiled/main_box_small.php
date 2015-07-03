4cd3925a9109bc869a7ebb0f5cc1bc5a4e8480ff
igunyaga <igunyaga@ubuntu12.vps>
2015-07-03 10:26:23 +0300

    first commit small

---
+++ include/compiled/main_box_small.php
@@ -0,0 +1,27 @@
+<div class="index_small">
+    <div class="small_top">
+        	<a href="/<?php echo $team['id'].'_'.$team['alias']; ?>"><img src="<?php if(1==preg_match('/http:/',$team['image']))echo $team['image']; 
+            else echo '/static/'.$team["image"]; ?>" alt="<?=$team['title']; ?>" width="380" border="0" height="213"/></a>
+    </div>
+    
+    
+    <!-- <a href="/team.php?id=<?php echo $team['id']; ?>"> -->
+    
+    
+    
+    <div class="small_bottom">
+        	<div class="small_bottom_txt">
+            <a href="/<?php echo $team['id'].'_'.$team['alias']; ?>"><?=$team['title'];?></a>
+            </div>
+            <div class="small_bottom_time" diff="<?php echo $diff_time; ?>000">
+             <?php //countdown($team['end_time']); ?>
+                    <span id="counter"><span class="date" rel="<?php echo ($team['end_time']); ?>"><?php countdown($team['end_time']); ?></span></span>
+            </div>
+            <div class="small_bottom_kupon">
+            <?php echo $all_days[$team['id']] ? $all_days[$team['id']]['q'] : 0; ?>
+            </div>
+            
+            <a href="/<?php echo $team['id'].'_'.$team['alias']; ?>" class="knopka_ok"><?=(moneyit($team['team_comission'])!=0?'Купить':'Получить')?></a>
+     </div>
+  
+</div>
